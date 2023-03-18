54e7346b74bcbabf4bcad669013affb905386a81
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-03-18 04:38:46 -0600

    03: Prepare the to-do database

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -1,17 +1,52 @@
 """This module provides the RP To-Do CLI."""
 
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from rptodo import __app_name__, __version__
+from rptodo import ERRORS, __app_name__, __version__, config, database
 
 app = typer.Typer()
+
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        '--db-path',
+        '-db',
+        prompt='to-do database location?',
+    )
+) -> None:
+    """Initialize the to-do database."""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating config file filed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+            f'The to-do database id {db_path}',
+            fg=typer.colors.GREEN,
+        )
+
 
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f'{__app_name__} v{__version__}')
         raise typer.Exit()
+
 
 @app.callback()
 def main(
